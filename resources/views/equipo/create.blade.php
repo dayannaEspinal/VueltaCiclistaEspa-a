1f26833b0d904e0af36df588e89c2b84ecb09e33
CAlessAgurElv <alessandroelvir826@gmail.com>
2026-03-18 01:23:00 -0600

    Update create.blade.php

--- resources/views/equipo/create.blade.php
+++ resources/views/equipo/create.blade.php
@@ -3,11 +3,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Crear Equipo</title>
 </head>
 <body>
-    <h1>Crear Equipo</h1>    
+    <h1>Crear Equipo</h1>
 
+    @if ($errors->any())
+        <div style="color:red;">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 
+    <form action="/equipo" method="POST">
+        @csrf
+        <label>Nombre:</label><br>
+        <input type="text" name="nombre" required maxlength="50"><br><br>
+
+        <label>Director:</label><br>
+        <input type="text" name="director" required maxlength="50"><br><br>
+
+        <label>Nacionalidad:</label><br>
+        <input type="text" name="nacionalidad" required maxlength="50"><br><br>
+
+        <button type="submit">Guardar</button>
+    </form>
+
+    <br>
+    <a href="/equipo">Volver a la lista</a>
 </body>
 </html>
