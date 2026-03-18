c96eeb9da1d20e26615998cb2f88afee64531ff5
CAlessAgurElv <alessandroelvir826@gmail.com>
2026-03-18 01:23:38 -0600

    Update edit.blade.php

--- resources/views/equipo/edit.blade.php
+++ resources/views/equipo/edit.blade.php
@@ -3,11 +3,38 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Editar Equipo</title>
 </head>
 <body>
-    <h1>Editar Equipo</h1>    
+    <h1>Editar Equipo</h1>
 
+    @if ($errors->any())
+        <div style="color:red;">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 
+    <form action="/equipo/{{ $equipo->id }}" method="POST">
+        @csrf
+        @method('PUT')
+
+        <label>Nombre:</label><br>
+        <input type="text" name="nombre" value="{{ $equipo->nombre }}" required maxlength="50"><br><br>
+
+        <label>Director:</label><br>
+        <input type="text" name="director" value="{{ $equipo->director }}" required maxlength="50"><br><br>
+
+        <label>Nacionalidad:</label><br>
+        <input type="text" name="nacionalidad" value="{{ $equipo->nacionalidad }}" required maxlength="50"><br><br>
+
+        <button type="submit">Actualizar</button>
+    </form>
+
+    <br>
+    <a href="/equipo">Volver a la lista</a>
 </body>
 </html>
