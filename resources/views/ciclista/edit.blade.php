444afe3944e6caeecea4cf04889f68db968e93c2
CAlessAgurElv <alessandroelvir826@gmail.com>
2026-03-18 01:11:03 -0600

    Update edit.blade.php

--- resources/views/ciclista/edit.blade.php
+++ resources/views/ciclista/edit.blade.php
@@ -3,11 +3,40 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Editar Ciclista</title>
 </head>
 <body>
-    <h1>Editar ciclista</h1>    
+    <h1>Editar Ciclista</h1>
 
+    
+    @if ($errors->any())
+        <div style="color:red;">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 
+    <form action="/ciclista/update" method="POST">
+        @csrf
+       
+        <input type="hidden" name="IdCiclista" value="{{ $ciclista->IdCiclista }}">
+
+        <label>Nombre:</label><br>
+        <input type="text" name="Nombre" value="{{ $ciclista->Nombre }}" required maxlength="50"><br><br>
+
+        <label>Nacionalidad:</label><br>
+        <input type="text" name="Nacionalidad" value="{{ $ciclista->Nacionalidad }}" maxlength="50"><br><br>
+
+        <label>Fecha de Nacimiento:</label><br>
+        <input type="date" name="FechaNacimiento" value="{{ $ciclista->FechaNacimiento }}"><br><br>
+
+        <button type="submit">Actualizar</button>
+    </form>
+
+    <br>
+    <a href="/ciclista">Volver a la lista</a>
 </body>
 </html>
