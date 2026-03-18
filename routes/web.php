dbec4324a64b3fbeaf6bde4766fc72129bacaded
Lidia504 <lidv_guifarroe@unicah.edu>
2026-03-18 17:17:29 -0600

    Agregando rutas CRUD de ciclista

--- routes/web.php
+++ routes/web.php
@@ -1,37 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\CiclistaController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-/* Rutas para la ciclista*/
-Route::get('/ciclista', function () {
-    return view('ciclista');
-});
-
-route::get('/ciclista/edit',function(){
-    return 'Estoy en la vista editar ciclista';
-});
-
-route::get('/ciclista/crear',function(){
-    return 'Estoy en la vista crear ciclista';
-});
-
-route::get('/ciclista/delet',function(){
-    return 'Estoy en la vista delete ciclista';
-});
-
-
+Route::get('/ciclista', [CiclistaController::class, 'index'])->name('ciclista.index');
+Route::get('/ciclista/create', [CiclistaController::class, 'create'])->name('ciclista.create');
+Route::post('/ciclista', [CiclistaController::class, 'store'])->name('ciclista.store');
+Route::get('/ciclista/{id}', [CiclistaController::class, 'show'])->name('ciclista.show');
+Route::get('/ciclista/{id}/edit', [CiclistaController::class, 'edit'])->name('ciclista.edit');
+Route::put('/ciclista/{id}', [CiclistaController::class, 'update'])->name('ciclista.update');
+Route::delete('/ciclista/{id}', [CiclistaController::class, 'destroy'])->name('ciclista.destroy');
+Route::get('/ciclista/{id}/delete', [CiclistaController::class, 'eliminar'])->name('ciclista.delete');
