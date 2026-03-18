abf9a3fe3b32ab30ed62606b1d94b01c433fbed0
CAlessAgurElv <alessandroelvir826@gmail.com>
2026-03-18 01:14:38 -0600

    Update delete.blade.php

--- resources/views/ciclista/delete.blade.php
+++ resources/views/ciclista/delete.blade.php
@@ -3,11 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Eliminar Ciclista</title>
 </head>
 <body>
-    <h1>Eliminar ciclista</h1>    
+    <h1>Eliminar Ciclista</h1>
 
+    @php
+        
+        $id = request()->query('id');
+        $ciclista = \App\Models\Ciclista::find($id);
+    @endphp
 
+    @if(!$ciclista)
+        <p style="color:red;">Ciclista no encontrado.</p>
+        <a href="/ciclista">Volver a la lista</a>
+    @else
+        <p>¿Está seguro que desea eliminar al ciclista <strong>{{ $ciclista->Nombre }}</strong>?</p>
+
+    
+        <form action="/ciclista/delet?id={{ $ciclista->IdCiclista }}" method="GET">
+            <button type="submit">Si, eliminar</button>
+        </form>
+
+        <br>
+        <a href="/ciclista">Cancelar y volver a la lista</a>
+    @endif
 </body>
 </html>
